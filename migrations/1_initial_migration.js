3d7c977b68badec77ce4d941b871924dee5398cd
Andrew LaPlante Johnson <55161807+laplantejohnsoaatwit@users.noreply.github.com>
2022-03-16 12:55:44 -0400

    first commit

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0x4c380f84F0314b0c9a3E68F686539a5097D1D1b9',
+    '1000000000000000000000'
+  )
+};
