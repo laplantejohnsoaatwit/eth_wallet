d9033fdae55901de408b93608e876f5cc9ecc232
Andrew LaPlante Johnson <55161807+laplantejohnsoaatwit@users.noreply.github.com>
2022-04-07 15:49:13 -0400

    Fixed Back end
    changed the addresses and also added the accounts to metamask

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
-    const daiTokenAddress = "0x7b729B07EcBDEd8879Acf997aAF6546926982830" // Replace DAI Address Here
+    const daiTokenAddress = "0x6869b3529bb3cA3055EEf9280Ed7E4279c40B2E8" // Replace DAI Address Here
     const daiTokenMock = new web3.eth.Contract(DaiTokenMock.abi, daiTokenAddress)
     this.setState({ daiTokenMock: daiTokenMock })
     const balance = await daiTokenMock.methods.balanceOf(this.state.account).call()
@@ -47,7 +47,7 @@ class App extends Component {
     this.state = {
       account: '',
       daiTokenMock: null,
-      balance: 0,
+      balance: 10,
       transactions: []
     }
 
